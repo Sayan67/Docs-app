e78547b8dd300716c2095380ce6fe1b785d726a0
Sayan67 <sayan.das.21@aot.edu.in>
2023-12-08 16:32:15 +0530

    Added all features and make the cards dragable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa"
 import { LuDownload } from "react-icons/lu"
-function Card() {
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import {motion} from 'framer-motion'
+function Card({data,reference}) {
     return (
-        <div className='overflow-hidden relative w-60 h-72 rounded-[35px] bg-zinc-900 text-zinc-400 p-8 text-xl'>
+        <motion.div drag dragConstraints={reference} whileDrag={{scale:1.1}} dragElastic={0.2} className='overflow-hidden relative w-60 h-72 rounded-[35px] bg-zinc-900 text-zinc-400 p-8 text-xl flex-shrink-0'>
             <FaRegFileAlt />
-            <p className='text-[0.9rem] mt-5 leading-tight'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, dignissimos Lorem ipsum dolor sit amet.</p>
+            <p className='text-[0.9rem] mt-5 leading-tight'>{data.title}</p>
             <div className='footer absolute left-0 bottom-0 w-full'>
                 <div className='flex w-full items-center justify-between px-8 mb-4'>
-                    <h5 className='text-white text-sm'>0.4mb</h5>
-                    <span className='rounded-full bg-zinc-500 p-[.4rem]'>
-                    <LuDownload size='0.8em' color='#fff' className='' />
+                    <h5 className='text-white text-sm'>{data.size}</h5>
+                    <span className='rounded-full bg-zinc-500 p-[.4rem] text-white'>
+                        {data.close?<IoMdCloseCircleOutline/>:<LuDownload size='0.8em' color='#fff' className='' />}
                     </span>
                 </div>
-                <div className='w-full bg-green-500 py-5'></div>
+                {data.tag.isTag ? (<div className={`w-full ${data.tag.color==='green'? "bg-green-500" : "bg-sky-500" } py-5`}></div>):null}
+                
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Card from './Card'
+import { useRef } from 'react';
 function Foreground() {
+  const data =[
+    {
+      title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, dignissimos Lorem ipsum dolor sit amet.",
+      size:"0.6mb",
+      close:false,
+      tag:{
+        isTag:true,color:"green",
+      }
+     
+    },
+    {
+      title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, dignissimos Lorem ipsum dolor sit amet.",
+      size:"0.6mb",
+      close:true,
+      tag:{
+        isTag:true,color:"sky",
+      }
+     
+    },
+    {
+      title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, dignissimos Lorem ipsum dolor sit amet.",
+      size:"0.6mb",
+      close:false,
+      tag:{
+        isTag:true,color:"green",
+      }
+     
+    },
+  ]
+  const Ref = useRef(null);
   return (
+
     <>
-        <div className='absolute top-0 z-[3] w-full h-screen '>
-            <Card/>  
+        <div ref={Ref} className='overflow-hidden flex gap-5 flex-wrap flex-shrink-0 absolute top-0 z-[3] w-full h-screen p-5'>
+            {data.map((item)=>(
+                <Card data={item} reference={Ref}/>)
+            )}
+             
         </div>
     </>
   )
